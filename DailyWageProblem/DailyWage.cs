0dc399a2f3f4c21556eeecd6e4299ff2f9553108
Arun37-Kumar <arun007624@gmail.com>
2022-04-03 13:39:14 +0530

    UC4 updated

--- DailyWageProblem/DailyWage.cs
+++ DailyWageProblem/DailyWage.cs
@@ -10,32 +10,36 @@ namespace DailyWageProblem
     {
         public void isPresent()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int IS_ABSENT = 0;
-            int EMP_RATE_PER_HOURS = 20;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int IS_ABSENT = 0;
+            const int EMP_RATE_PER_HOURS = 20;
 
             //variables
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0,3);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
+                case IS_PART_TIME:
+                    empHrs = 8;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_ABSENT:
+                    empHrs = 0;
+                    break;
+                default:
+                    break;
             }
-            else if(empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+
             empWage = empHrs * EMP_RATE_PER_HOURS;
-            Console.WriteLine("Emp Wage : {0}",empWage);
+            Console.WriteLine("Emp Wage : {0}", empWage);
 
         }
     }
