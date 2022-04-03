6d2369c3f841d34e2b311ce806dc43410b74c5d0
Arun37-Kumar <arun007624@gmail.com>
2022-04-03 14:59:27 +0530

    commit

--- DailyWageProblem/DailyWage.cs
+++ DailyWageProblem/DailyWage.cs
@@ -14,12 +14,17 @@ namespace DailyWageProblem
             const int IS_PART_TIME = 2;
             const int IS_ABSENT = 0;
             const int EMP_RATE_PER_HOURS = 20;
+            const int NUMBER_OF_WORKING_DAYS = 20;
 
             //variables
             int empHrs = 0;
             int empWage = 0;
+            int totalWage = 0;
 
             Random random = new Random();
+           
+            for(int days = 0; days < NUMBER_OF_WORKING_DAYS; days++)
+            {
             int empCheck = random.Next(0, 3);
 
             switch (empCheck)
@@ -37,9 +42,12 @@ namespace DailyWageProblem
                 default:
                     break;
             }
-
+                totalWage += empWage;
             empWage = empHrs * EMP_RATE_PER_HOURS;
+            
             Console.WriteLine("Emp Wage : {0}", empWage);
+            }
+            Console.WriteLine("Total Employee Wage : "+totalWage); 
 
         }
     }
